ce104aa1cafbfdafccde869d27807bed4857f07e
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-11-03 15:55:54 -0300

    Create project maven

---
+++ send-email/src/main/java/br/com/send/email/classes/App.java
@@ -0,0 +1,13 @@
+package br.com.send.email.classes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ send-email/src/test/java/br/com/send/email/classes/AppTest.java
@@ -0,0 +1,17 @@
+package br.com.send.email.classes;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest { 
+
+	
+	@org.junit.Test
+	public void testEmail() {
+		
+	}
+}
