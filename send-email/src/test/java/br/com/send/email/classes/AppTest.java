267949dfbc237ebd17d19052b6b1730e34e58bf5
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-11-03 17:12:12 -0300

    Settings the properties of connections

--- send-email/src/test/java/br/com/send/email/classes/AppTest.java
+++ send-email/src/test/java/br/com/send/email/classes/AppTest.java
@@ -1,5 +1,7 @@
 package br.com.send.email.classes;
 
+import java.util.Properties;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -13,5 +15,12 @@ public class AppTest {
 	@org.junit.Test
 	public void testEmail() {
 		
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true"); //Authorization 
+		properties.put("mail.starttls","true"); //Authentication
+		properties.put("mail.smtp.host","smtp.gmail.com"); //Server Google Gmail
+		properties.put("mail.smtp.port", "465"); //Server Door 
+		properties.put("mail.smtp.socketFactory.port","465"); //Specifies the port to be connected by socket
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); //Class that connects socket to SMTP
 	}
 }
