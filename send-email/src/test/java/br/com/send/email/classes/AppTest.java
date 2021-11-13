404c8e97f8db39536fa10799674fd781cbb91176
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-11-12 21:48:12 -0300

    Email costumer with Html

--- send-email/src/test/java/br/com/send/email/classes/AppTest.java
+++ send-email/src/test/java/br/com/send/email/classes/AppTest.java
@@ -22,19 +22,20 @@ public class AppTest {
 
 	@org.junit.Test
 	public void testEmail() throws Exception {
-		
-		StringBuilder stringBuilderTextEmail = new StringBuilder();
-		
-		stringBuilderTextEmail.append("Hi, <br><br>");
-		stringBuilderTextEmail.append("<h2>You are receiving the Java course access.</h2>");
 
-		SendEmailObject sendEmailObject = new SendEmailObject(
-				"wiltamar@cagepa.pb.gov.br", 
-				"Wyltamar", 
-				"Send email with Java", 
-				stringBuilderTextEmail.toString()
-				);
+		StringBuilder stringBuilderTextEmail = new StringBuilder();
+
+		stringBuilderTextEmail.append("Hi, <br><br>");
+		stringBuilderTextEmail.append("<p>You are receiving the the link for project Virtual Store.</p><br>");
+		stringBuilderTextEmail.append("<p><b>Login:</b> wyltamar83</p>");
+		stringBuilderTextEmail.append("<p><b>Senha:</b> y33eej87dd</p><br><br>");
+		stringBuilderTextEmail.append(
+				"<a href=\"https://github.com/wyltamar/loja_virtual_mentoria\" style=\"color:#2525a7; padding: 14px 25px; text-align:center; text-decoration: none; display: inline-block; border-radius:30px; font-size:20px; font-family:courier; border:3px solid green; background-color:#99DA39;\">Access Stuent Portal</a><br><br>");
 		
+		stringBuilderTextEmail.append("<span style=\"font-size: 9px\">Ass.: Wyltamar Douglas de Sousa Oliveira</spam>");
+		SendEmailObject sendEmailObject = new SendEmailObject("wyltamar.douglasdesousaoliveir@gmail.com", "Wyltamar",
+				"Send email with Java", stringBuilderTextEmail.toString());
+
 		sendEmailObject.sendEmail(true);
 
 		Thread.sleep(5000); // await 5 seconds for closed the test
