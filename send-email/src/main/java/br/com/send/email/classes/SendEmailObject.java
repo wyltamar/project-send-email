2bb304780c2962b24f1f199289790b9f0a42a5fe
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-11-13 06:14:12 -0300

    Creating the simulation of file pdf

--- send-email/src/main/java/br/com/send/email/classes/SendEmailObject.java
+++ send-email/src/main/java/br/com/send/email/classes/SendEmailObject.java
@@ -1,5 +1,8 @@
 package br.com.send.email.classes;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.Properties;
 
 import javax.mail.Address;
@@ -11,27 +14,31 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 public class SendEmailObject {
-	
+
 	private String userName = "wyltamarjavadev@gmail.com";
-	private String password = "javadev123"; //password here is incorrect
-	
+	private String password = "javadev123"; // password here is incorrect
+
 	private String ricipient = "";
 	private String submitter = "";
 	private String subject = "";
 	private String text = "";
-	
+
 	public SendEmailObject(String ricipient, String submitter, String subject, String text) {
 		this.ricipient = ricipient;
 		this.submitter = submitter;
 		this.subject = subject;
 		this.text = text;
 	}
-	
-	public void sendEmail(boolean envioHtml) throws Exception{
-		
+
+	public void sendEmail(boolean envioHtml) throws Exception {
+
 		Properties properties = new Properties();
-		
+
 		properties.put("mail.smtp.ssl.trust", "*"); // Authentication ssl
 		properties.put("mail.smtp.auth", "true"); // Authorization
 		properties.put("mail.starttls", "true"); // Authentication
@@ -49,23 +56,38 @@ public class SendEmailObject {
 			}
 		});
 
-		Address[] toUser = InternetAddress 
-				.parse(ricipient);
-		
+		Address[] toUser = InternetAddress.parse(ricipient);
+
 		Message message = new MimeMessage(session);
-		message.setFrom(new InternetAddress(userName,submitter)); // quem estar enviadno
-		message.setRecipients(Message.RecipientType.TO, toUser); //email de destino
-		message.setSubject(subject); //assunto do email
-		
-		if(envioHtml) {
+		message.setFrom(new InternetAddress(userName, submitter)); // quem estar enviadno
+		message.setRecipients(Message.RecipientType.TO, toUser); // email de destino
+		message.setSubject(subject); // assunto do email
+
+		if (envioHtml) {
 			message.setContent(text, "text/html; chartset=utf-8");
-		}else {
-			message.setText(text); //corpo do email
-			
+		} else {
+			message.setText(text); // corpo do email
+
 		}
-		
+
 		Transport.send(message);
 
+	}
+
+	/* Mock file pdf 
+	 * Return a PDF with the paragraph text */
+	
+	private FileInputStream pdfSimulator() throws Exception {
+
+		Document documentPdf = new Document();
+		File file = new File("attachmentFile.pdf");
+		file.createNewFile();
+		PdfWriter.getInstance(documentPdf, new FileOutputStream(file));
+		documentPdf.open();
+		documentPdf.add(new Paragraph("This line will be writer within PDF file"));
+		documentPdf.close();
+
+		return new FileInputStream(file);
 	}
 
 }
